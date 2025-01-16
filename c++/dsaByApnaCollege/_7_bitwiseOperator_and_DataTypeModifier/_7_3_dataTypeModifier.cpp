32a6bfa4cea39e75e6c309bb0e81b96212087f62
Scary-Spark <scary.spark.2003@gmail.com>
2025-01-16 20:33:30 +0600

    day 1

---
+++ c++/dsaByApnaCollege/_7_bitwiseOperator_and_DataTypeModifier/_7_3_dataTypeModifier.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    /*
+        TODO:  Data type modifier
+            ?   it's change the meaning of a existing data type.
+
+            ?   Data type modifiers:
+                    1. long
+                    2. short
+                    3. long long
+                    4. signed
+                    5. unsigned
+    */
+}
